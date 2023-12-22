b61627e5cdd6d18aec6c5dcc6456c9112f641fec
Sulavjung <79497159+Sulavjung@users.noreply.github.com>
2023-12-22 13:51:39 -0800

    2. Intermediary: Showing what not to do while creating same view multiple times. Create another view right below the original view.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -12,31 +12,73 @@ import SwiftData
 
 struct ContentView: View {
     
-    
-    //Some view here means that: The compiler will get the computed value and determine which view it is and assign it appropriate view to the var body.
-    //WHy var body? -> This is required field of the View protocol. That is why we should have the var body of some view in the ContentView here.
-    //Some view allow us to have a complex view using other view. It is like lego and that is possible until and unless the return is some view.
-    var body: some View {           //The curly braces starting here to the end of it is called computed properties.
+    var body: some View {
         
-        
-        
-        //====Creating instance of structs:=====
-        //This is perfect example. The Image and Test. On Image we use the Image and in the (parameters goes here).
-        //Parameter Defaults:
-        //What about the VStack then? -> Why does the VStack don't have the open parameter and something? The thing is it could have. So, having no argument doesn't always meant you can do VStack just that. You have to have VStack() like that.
-        //But then Why VStack got the curly braces things below? -> These things in curly braces are actually ====EMBEDED FUNCTIONS==== that are being passed as parameter. What we are actually doing is: VStack(content: { this }) but simplified to this view.
-        //Then what are those .imageScale and .foregroundColor. Those are function related to structs. We call those functions like that on structs. Those are also called =====VIEW MODIFIER====. Those could be chained and each function returns a view and then that view is then further modified with the modifier.
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.orange)
-            Text("Hello World!")
+        HStack {
+            
+            ZStack {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            }
+            ZStack {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            }
+            ZStack {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            }
+            ZStack {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            }
         }
         .padding()
+        .foregroundColor(.orange)
     }
 
     
 }
+
+
+
+struct CardView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 12)
+                .foregroundColor(.white)
+            RoundedRectangle(cornerRadius: 12)
+                .strokeBorder(lineWidth: 2)
+            Text("👻").font(.largeTitle)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #Preview {
     ContentView()
