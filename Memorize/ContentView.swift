edc2b85ab0ff7cbb7330f4eea7ff85dcd76e0679
Sulavjung <79497159+Sulavjung@users.noreply.github.com>
2023-12-22 14:50:59 -0800

    5. Creating array in the Swift. Different ways explained in the comment above the actual used.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -11,13 +11,16 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
+    //====Creating array in the Swift====
+    //You can use let emojis: Array<String> = [] or you can also use let emojis: [String][] or you can just allow inference to take care of it by using it like below. 
+    let emojis = ["😀", "😃", "😍", "🤪", "🥰", "😉"]
     
     var body: some View {
         HStack {
-            CardView(isFaceUp: true)
-            CardView(isFaceUp: true)
-            CardView(isFaceUp: true)
-            CardView(isFaceUp: true)
+            CardView(content: emojis[0], isFaceUp: true)
+            CardView(content: emojis[1], isFaceUp: true)
+            CardView(content: emojis[2], isFaceUp: true)
+            CardView(content: emojis[3], isFaceUp: true)
         }
         .padding()
         .foregroundColor(.orange)
@@ -25,29 +28,20 @@ struct ContentView: View {
 }
 
 struct CardView: View {
+    let content: String
     @State var isFaceUp = false
     var body: some View {
-        
-        
-        //====ViewBuilder==== can have variables. It can only have list, local variables, and conditionals.
-        //====let vs var: ==== var base: RoundedRectangle = RoundedRectangle(cornerRadius: 12) -> We never say this: Because var means it will change but we will never change this variable. So, we will use let. So, we mostly only use let in viewBuilder.
-        //====Type Inference: ===== let base: RoundedRectangle = RoundedRectangle(cornerRadius: 12) -> So, we dont do this either even when it is correct. The reason, we let swift decide to infer what it is. Swift can tell what it is seing what it is returning. Since, RoundedRectangel returns just RoundedRectange, the swift auto infer the variable to be the RoundedRectangle.
-        //We do similar thing to the above isFaceUp. We can remove the : Bool.
-        
         ZStack {
             let base = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
-                Text("👻").font(.largeTitle)
+                Text(content).font(.largeTitle)
             }else {
                 base.fill()
             }
         }
         .onTapGesture{
-            //We can't do isFaceUp = !isFaceUp alone becuase self is immutable. So, we have to add @State to var isFaceUp.
-            //What the ====@State==== Does then? -> The state creates a pointer to the memory that hold the isFaceUp.
-            //So we could also do isFaceUp.toggle() which is available for the Bool struct in swift.
             isFaceUp.toggle();
         }
     }
